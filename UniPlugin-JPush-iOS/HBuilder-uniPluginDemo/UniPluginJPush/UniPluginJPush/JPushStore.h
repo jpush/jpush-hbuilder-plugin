96e31e68566dc333aeedc9321543c18901385ddc
huangshuni <15889638723@163.com>
2022-09-02 16:33:04 +0800

    iOS升级JPush SDK版本为4.8.1

--- UniPlugin-JPush-iOS/HBuilder-uniPluginDemo/UniPluginJPush/UniPluginJPush/JPushStore.h
+++ UniPlugin-JPush-iOS/HBuilder-uniPluginDemo/UniPluginJPush/UniPluginJPush/JPushStore.h
@@ -30,14 +30,11 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface JPushStore : NSObject <JPUSHRegisterDelegate, JPUSHGeofenceDelegate,JPushInMessageDelegate,PKPushRegistryDelegate>
+@interface JPushStore : NSObject <JPUSHRegisterDelegate, JPUSHGeofenceDelegate,PKPushRegistryDelegate>
 
 @property (nonatomic, copy) UniModuleKeepAliveCallback connectEventCallback; // 连接状态
 @property (nonatomic, copy) UniModuleKeepAliveCallback pushNotiCallback; // apns推送消息
 @property (nonatomic, copy) UniModuleKeepAliveCallback receiveCustomNotiCallback; //自定义消息
-
-@property (nonatomic, assign) BOOL allowedInMessagePop;// 是否允许应用内消息弹出
-@property (nonatomic, copy) UniModuleKeepAliveCallback inMessageCallback; // 应用内消息(ad and 应用内通知)
 
 @property (nonatomic, copy) UniModuleKeepAliveCallback localNotiCallback; // 本地通知
 
