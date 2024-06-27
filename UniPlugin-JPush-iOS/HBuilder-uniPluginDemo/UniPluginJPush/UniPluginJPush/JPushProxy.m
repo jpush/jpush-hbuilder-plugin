7e845502414ae8bd04f6007bd539052ec8c274ed
huangshuni <15889638723@163.com>
2024-06-27 15:26:23 +0800

    修复iOS点击通知冷启动app时偶现不回调的问题

--- UniPlugin-JPush-iOS/HBuilder-uniPluginDemo/UniPluginJPush/UniPluginJPush/JPushProxy.m
+++ UniPlugin-JPush-iOS/HBuilder-uniPluginDemo/UniPluginJPush/UniPluginJPush/JPushProxy.m
@@ -13,11 +13,11 @@
 @implementation JPushProxy
 
 - (void)onCreateUniPlugin {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
 }
 
 - (BOOL)application:(UIApplication *_Nullable)application didFinishLaunchingWithOptions:(NSDictionary *_Nullable)launchOptions {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
     
     [JPushStore shared].launchOptions = launchOptions;
     
@@ -33,27 +33,27 @@
 
 
 - (void)applicationWillResignActive:(UIApplication * _Nullable)application {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *_Nullable)application {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *_Nullable)application {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *_Nullable)application {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
 }
 
 - (void)applicationWillTerminate:(UIApplication *_Nullable)application {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
     [JPUSHService registerDeviceToken:deviceToken];
     [[JPushStore shared] handleDeviceTokenSuccess:deviceToken];
 }
@@ -64,7 +64,7 @@
 
 // 推送唤醒会走这里
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    NSLog(@"UniPluginProtocol Func: %@,%s",self,__func__);
+    NSLog(@"JPushModule--->UniPluginProtocol Func: %@,%s",self,__func__);
     [[JPushStore shared] handeleApnsCallback:userInfo type:NOTIFICATION_ARRIVED];
 }
 
